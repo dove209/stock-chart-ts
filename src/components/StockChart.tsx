fc6a98037f2ae5fef4045c4db0a64045f08960e9
GINI <dove209@naver.com>
2022-07-08 11:50:06 +0900

    Update StockChart.tsx

--- src/components/StockChart.tsx
+++ src/components/StockChart.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useCallback, useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
